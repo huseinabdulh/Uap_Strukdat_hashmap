240011029691823c472da67682792d0870f0c834
ASUS RYZEN <ASUS RYZEN@LAPTOP-9PMMSI13>
2019-12-18 18:45:44 +0700

    Coba dulu boss

--- src/Modul3/main.java
+++ src/Modul3/main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Modul4;
+package Modul3;
 
 import java.util.HashMap;
 import java.util.Map;
